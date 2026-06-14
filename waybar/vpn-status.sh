156b688cf8ba18f8c79e6f1c07e30f3cbe305aa5
Jed Blenkhorn <hello@jedbillyb.com>
2026-06-15 10:38:14 +1200

    Colour VPN bar module green when on

--- waybar/vpn-status.sh
+++ waybar/vpn-status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if ip link show type wireguard 2>/dev/null | grep -q 'wg'; then
-    echo "vpn on"
+    echo '{"text":"vpn on","class":"on"}'
 else
-    echo "vpn off"
+    echo '{"text":"vpn off","class":"off"}'
 fi
